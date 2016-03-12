37fe7cec595d01c57dbd252ddaa45e427300f649
Nathan Ross Powell <nathanrosspowell@gmail.com>
2016-03-12 14:10:35 -0500

    Full data dump from the module

--- gofundme.js
+++ gofundme.js
@@ -2,33 +2,50 @@ var phantom = require('phantom');
 var cheerio = require('cheerio');
 var merge = require('merge'), original, cloned;
 
-function logNames(html) {
-    //console.log(html);
+function getNumber(value) {
+    var goal = value.toString()
+    var multiplier = goal[goal.length-1];
+    var goalNumber = 0;
+    if (multiplier === 'k') {
+        goalNumber = parseInt(goal.replace(/[,.]/g, ""));
+        goalNumber *= 1000;
+    }
+    else if (multiplier === 'm'){
+        goalNumber = parseFloat(goal.replace(/[,]/g, "."));
+        goalNumber *= 1000000
+    }
+    else {
+        goalNumber = parseInt(goal.replace(/[,.]/g, ""));
+    }
+    return goalNumber
+}
+
+function getDonorData(html) {
     var $ = cheerio.load(html);
     var nums = $('div > div.donerscroll > span:nth-child(3)').text();
     var startNum = parseInt(nums.split("-")[0])
     var i = 0;
-    var donators = {}
+    var donors = {}
     $('div.doner').each(function(index, element){
         var index = (startNum + i ) 
         var amountHtml = $(element).find("div.damt").text()
         var numb = amountHtml.match(/\d/g);
         var amount = parseInt(numb.join(""))
-        donators[index] = {
+        donors[index] = {
             "name" : $(element).find("div.dname").text(),
             "message" : $(element).find("div.dcom").text(),
             "amount" : amount
         }
         i += 1;
     });
-    return donators
+    return donors
 }
 
-function recurseNames(page, callback, preDonators) {
+function recurseDonors(page, callback, preDonators) {
     page.evaluate(function() {
         return document.querySelector('div[class="donate_right"]').innerHTML
     }).then(function(html){
-        var donators = merge(logNames(html), preDonators);
+        var donors = merge(getDonorData(html), preDonators);
         page.evaluate(function(){
             var a = document.querySelector("div > div.donerscroll > a.lr.pright.donationsPage");
             var finished = a === null;
@@ -45,29 +62,48 @@ function recurseNames(page, callback, preDonators) {
                     page.evaluate(function() {
                         return document.querySelector('div[class="donate_right"]').innerHTML;
                     }).then(function(html){
-                        recurseNames(page, callback, donators);
+                        recurseDonors(page, callback, donors);
                     });
                 }, 500);
             }
             else{
-                callback(donators);
+                callback(donors);
             }
         });
     });
 }
 
-function getDonators(goFundMeURL, callback) {
+function goFundMe(input, callback) {
     phantom.create().then(function(ph) {
         ph.createPage().then(function(page) {
-            page.open(goFundMeURL).then(function(status) {
+            page.open(input.url).then(function(status) {
                 page.property('content').then(function(content) {
-                    var $ = cheerio.load(content);
-                    var title = $('.pagetitle').text();
-                    recurseNames(page, function(donators){
+                    var $ = cheerio.load(content),
+                            title = $('.pagetitle').text(),
+                            currency = $('div.raised > span.cur').text(),
+                            goal = $('div.raised > span.goal').text().replace(currency, ""),
+                            goalNumber = getNumber(goal),
+                            amountHtml = $("div.raised").text()
+                    var numb = amountHtml.split('of')[0].match(/\d/g);
+                    var amount = parseInt(numb.join(""))
+                    var amountNumber = getNumber(amount)
+                    var percentage = amountNumber / (goalNumber / 100 )
+                    recurseDonors(page, function(donors){
                         page.close();
                         ph.exit();
                         if ( callback !== "undefined" ) {
-                            callback(donators)
+                            
+                            var data = {
+                                title: title,
+                                currency: currency,
+                                amount: amount,
+                                amountNumber: amountNumber,
+                                goal: goal,
+                                goalNumber: goalNumber,
+                                percentage: percentage,
+                                donors: donors
+                            }
+                            callback(data)
                         }
                     });
                 });
@@ -76,4 +112,4 @@ function getDonators(goFundMeURL, callback) {
     });
 }
 
-module.exports = getDonators
+module.exports = goFundMe
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var GetDoners = require('./gofundme');
-var goFundMeURL = 'https://www.gofundme.com/39t6wr3c' // 'https://www.gofundme.com/samyancey' //'https://www.gofundme.com/ShowerFlint5' 
-GetDoners(goFundMeURL, function(donators){
-    for ( i in donators ) {
-        console.log( " > " + i + " ", donators[i] );
+var GoFundMe = require('./gofundme');
+var input = {
+    url: 'https://www.gofundme.com/39t6wr3c' // 'https://www.gofundme.com/samyancey' //'https://www.gofundme.com/ShowerFlint5' 
+}
+GoFundMe(input, function(data){
+    console.log("Title: ", data.title);
+    console.log("Raised: " + data.currency.toString() + data.amount.toString() + " of " + data.currency.toString() + data.goal.toString());
+    console.log("Percentage: " + data.percentage.toString())
+    console.log("Donors: ");
+    for ( i in data.donors ) {
+        console.log("   " + i + ". " + data.donors[i].name + " - " + data.donors[i].amount)
     }
 });
 
